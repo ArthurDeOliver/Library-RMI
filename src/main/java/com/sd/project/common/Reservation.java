80d97d9aa66ee7326922a9b3fb9d9d7a6ccda9a0
Karen Ribeiro <kevr@discente.ifpe.edu.br>
2025-07-14 21:39:47 -0300

    feature: models

---
+++ src/main/java/com/sd/project/common/Reservation.java
@@ -0,0 +1,39 @@
+package com.sd.project.common;
+
+import java.io.Serializable;
+
+public class Reservation implements Serializable {
+    private int id;
+    private String reservationDate;
+    private String userId;
+    private String bookIsbn;
+
+    public Reservation(int id, String userId, String bookIsbn, String reservationDate) {
+        this.id = id;
+        this.userId = userId;
+        this.bookIsbn = bookIsbn;
+        this.reservationDate = reservationDate;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getBookIsbn() {
+        return bookIsbn;
+    }
+
+    public String getReservationDate() {
+        return reservationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                "userId='" + userId + '\'' +
+                ", bookIsbn='" + bookIsbn + '\'' +
+                ", reservationDate='" + reservationDate + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/sd/project/common/User.java
@@ -0,0 +1,36 @@
+package com.sd.project.common;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String userId;
+    private String name;
+    private String email;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
